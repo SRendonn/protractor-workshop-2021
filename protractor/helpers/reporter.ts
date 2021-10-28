9432c0ded60c24e9af7fbdb4c0613fb65ce97023
Sebastian Rendon <sebasrg0505@gmail.com>
2021-10-28 01:32:29 -0500

    added jasmine-awesome-report

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,11 +1,11 @@
-import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+const { AwesomeReport } = require('jasmine-awesome-report');
+
+const config = {
+  fullPath: 'awesome-report',
+  fileName: 'report',
+  merge: true,
+};
 
 export const reporter = () => {
-  jasmine.getEnv().addReporter(
-    new SpecReporter({
-      spec: {
-        displayStacktrace: StacktraceOption.PRETTY,
-      },
-    }),
-  );
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
